57345f9763a7c2c682f46d251359372b61572322
MUTEJIMMY <160954410+MUTEJIMMY@users.noreply.github.com>
2026-04-30 23:25:19 -0400

    Add: longest-common-prefix - submission-113

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-113.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+         
+        String answer = "";
+    
+        for(int i = 0; i < strs[0].length(); i++){
+
+            String checker = "";
+            Boolean bool = true;
+
+            for(int j = 0; j < strs.length; j++){
+                System.out.println("hello");
+                System.out.println(j);
+
+
+                checker += strs[j].charAt(i);
+                System.out.println(checker);
+
+                if(checker.charAt(0) == strs[j].charAt(i)){
+                    bool = false;
+                }
+            }
+            
+            if(bool){
+                answer += strs[0].charAt(i);
+            }
+            else{
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
