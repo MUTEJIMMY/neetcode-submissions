4055644a184232d715634f808591990422022f08
MUTEJIMMY <160954410+MUTEJIMMY@users.noreply.github.com>
2026-05-01 19:39:44 -0400

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        //int[] arr = new int[nums.length];
+        int k = 0;
+        int tracker = nums.length;
+        int inbetween = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == val){
+                
+                inbetween = nums[nums.length-tracker];
+                nums[nums.length-tracker] = nums[i];
+                nums[i] = inbetween;
+                tracker --;
+                System.out.println(tracker + "  " + inbetween );
+
+            }
+            else{
+                k++;
+                System.out.println("IN the k loop");
+            }
+        }
+        System.out.println(k);
+        return k;
+    }
+}
