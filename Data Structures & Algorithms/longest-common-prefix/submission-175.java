3f6bceb3df74e3a686b18f7a8ad9602e01e6abf3
MUTEJIMMY <160954410+MUTEJIMMY@users.noreply.github.com>
2026-05-01 17:50:31 -0400

    Add: longest-common-prefix - submission-175

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-175.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+         
+        String answer = "";
+
+        for(int i = 0; i < strs[0].length(); i++){
+            //System.out.println("here is strs[0].length(): " + strs[0].length());
+            String checker = "";
+            Boolean bool = true;
+
+            System.out.println("here is the i for this loop: " + i);
+            System.out.println("Entering loop");
+            for(int j = 0; j < strs.length; j++){
+
+                System.out.println("here is the j for this loop: " + j);
+                try{
+                System.out.println("here is strs[j].charAt(i): " + strs[j].charAt(i));
+
+                checker += strs[j].charAt(i);
+
+                System.out.println("Here is checker: " + checker);
+
+                
+            if(checker.charAt(0) != checker.charAt(checker.length()-1)){
+                bool = false;
+                }
+                }
+                catch(Exception e){
+                    return answer;
+                }
+                System.out.println("");
+            }
+            System.out.println("Exiting Loop");
+            if(bool){
+                answer += strs[0].charAt(i);
+            }
+            else{
+                break;
+            }
+            
+        }
+        //System.out.println("Hello");
+        return answer;
+    }
+}
