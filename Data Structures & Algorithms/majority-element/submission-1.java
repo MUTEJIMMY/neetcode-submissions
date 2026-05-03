f5b31fbe36e7d8cb75344e716e9d7e194cda5840
MUTEJIMMY <160954410+MUTEJIMMY@users.noreply.github.com>
2026-05-02 22:12:38 -0400

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int counter = 0;
+        int max = 0;
+        int answer = 0;
+        if(nums.length > 1){
+        for(int i = 0; i < nums.length; i++){
+            if(hm.containsKey(nums[i])){
+                int x = hm.get(nums[i]);
+                hm.put(nums[i], x+1);
+                counter = x+1;
+                if(max < counter ){
+                    max = counter;
+                    answer = nums[i];
+                }
+            }
+            else{
+                hm.put(nums[i], 1);
+            }
+        }
+        System.out.println(hm);
+        return answer;
+        }
+        else{
+            return nums[0];
+        }
+    }
+}
